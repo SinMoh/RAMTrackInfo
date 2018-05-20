e692b12b3622b98560f67c3c2c787b5a229c6b23
Mouhcine BENMEZIANE <bensinmoh@MacSin.local>
2018-05-20 22:07:47 +0000

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AddTripPage } from "../add-trip/add-trip";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  NAVTO_AddTrip() {
+    this.navCtrl.push(AddTripPage)
   }
 
 }
